98cfd3f39b7454ab308406d4fe754dc1eb69ddd5
MDrevon <MsDrevon@gmail.com>
2023-02-28 14:51:57 -0600

    Added guessAgain function and tracked attempts

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
 //console.log("This is linked")
 
 var secretNum = 42;
+var attempts = 0;
 
 let yourNum = guess()
 
 function guess()
 {
+
     let guessNum = prompt("Enter your guess:")
     if(guessNum < secretNum)
     {
-        alert("Higher");
+        //alert("Higher");
+        attempts = attempts + 1;
+        guessAgain("Higher");
     }
     else if(guessNum > secretNum)
     {
-        alert("Lower");
+        //alert("Lower");
+        attempts = attempts + 1;
+        guessAgain("Lower");
+
     }
     else
     {
-        alert("Correct!")
+        attempts = attempts + 1;
+        alert("Correct! It took you " + attempts + " guesses");
+    }
+}
+
+function guessAgain(status)
+{
+    let guessNum = prompt(status + " Guess again");
+
+    if(guessNum < secretNum)
+    {
+        //alert("Higher");
+        attempts = attempts + 1;
+        guessAgain("Higher");
+    }
+    else if(guessNum > secretNum)
+    {
+        //alert("Lower");
+        attempts = attempts + 1;
+        guessAgain("Lower");
+
+    }
+    else
+    {
+        attempts = attempts + 1;
+        alert("Correct! It took you " + attempts + " guesses");
     }
 }
